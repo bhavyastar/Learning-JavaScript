de4bb0c8b7d915d814bb7fd8dc3a1bd36e1c91f3
Bhavya <bhavya.sachdeva.58@gmail.com>
2021-11-19 23:36:36 +0530

    javascript added

---
+++ Day1/Calculator/index.js
@@ -0,0 +1,29 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el") 
+function add()
+ {
+     let result = num1 + num2
+     sumEl.textContent = "Sum:" + result
+ }
+
+ function sub()
+ {
+    let result = num1 - num2
+    sumEl.textContent = "Sub:" + result
+
+ }
+ function mul()
+ {
+    let result = num1 * num2
+    sumEl.textContent = "Mul:" + result
+
+ }
+ function div()
+ {
+    let result = num1 / num2
+    sumEl.textContent = "Div:" + result
+
+ }
