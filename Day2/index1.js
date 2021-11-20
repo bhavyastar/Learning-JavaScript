b2fec71cf690acab145205799dea571bb970d4a4
Bhavya <bhavya.sachdeva.58@gmail.com>
2021-11-20 20:38:56 +0530

    Complete Day2

--- Day2/index1.js
+++ Day2/index1.js
@@ -1,19 +1,43 @@
-let firstCard = 10
-let secondCard = 30
-let cards = [firstCard , secondCard]
-let sum = firstCard + secondCard 
+let player = {
+    name1: "Per" ,
+    chips: 200
+}
+let cards = []
+let sum = 0 
 let hasBlackJack = false
 let isAlive = true
-let message = " "
+let message = ""
 let messageEl  = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name1 + ": $" + player.chips
+function getRandomcard() {
+ let randomNumber = Math.floor(Math.random()*32) + 1
+ if(randomNumber>10){
+     return 10
+ } else if (randomNumber === 1) {
+     return 11
+ }
+ else {
+     return randomNumber
+ }
+}
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 function renderGame()
 {
-     cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+     cardsEl.textContent = "Cards: " 
+     for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
      sumEl.textContent = "Sum: " + sum
     if(sum<=21)
      {
@@ -33,10 +57,13 @@ messageEl.textcontent = message
 }
 function newCard()
 {
-    console.log("Drawing a new card from the deck!")
-    let card = 7
-    sum += card
-    renderGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomcard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
 }
+
 
 
